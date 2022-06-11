bbdaede3a7b0d3717e8e0e5b4263b8ffde071d0a
DenisMuga <denismugah5@gmail.com>
2022-06-11 13:24:50 +0300

    Creating a user profile serializer with corresponding fields

--- mugapp/serializers.py
+++ mugapp/serializers.py
@@ -3,3 +3,7 @@ from .models import Profile, Post
 from django.contrib.auth.models import User
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
