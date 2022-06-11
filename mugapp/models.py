a3f1b16a247121fffe73b73d50abc7373e673d37
DenisMuga <denismugah5@gmail.com>
2022-06-11 12:23:54 +0300

    Adding a profile return string

--- mugapp/models.py
+++ mugapp/models.py
@@ -6,5 +6,12 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = CloudinaryField('images')
+    bio = models.TextField(max_length=600, default='Fantastic projects over here', blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
 
