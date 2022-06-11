1864fe1e30304b6dc1e89e5cecb176597fafe3f4
DenisMuga <denismugah5@gmail.com>
2022-06-11 13:27:38 +0300

    Creating a profile and user serializers with their fields

--- mugapp/serializers.py
+++ mugapp/serializers.py
@@ -13,3 +13,11 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
+        
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'url', 'username', 'profile', 'posts']
