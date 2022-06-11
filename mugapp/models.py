8802b2aef274244bfb6e16260482ac6ce0187a64
DenisMuga <denismugah5@gmail.com>
2022-06-11 12:19:15 +0300

    Creating a profile model

--- mugapp/models.py
+++ mugapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = CloudinaryField('images')
+
+
