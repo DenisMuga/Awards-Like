abc8b2788486b491b11eb8bbb56b4c921de61939
DenisMuga <denismugah5@gmail.com>
2022-06-11 12:33:46 +0300

    Creating model for user post in models py

--- mugapp/models.py
+++ mugapp/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+import datetime as dt
 
 
 # Create your models here.
@@ -13,5 +18,18 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    
+class Post(models.Model):
+    title = models.CharField(max_length=155)
+    url = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+    technologies = models.CharField(max_length=200, blank=True)
+    photo =CloudinaryField('images') 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
 
 
