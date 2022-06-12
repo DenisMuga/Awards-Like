4c3d91a9d35d40ee76e87beef77781c202b9b544
DenisMuga <denismugah5@gmail.com>
2022-06-12 07:37:35 +0300

    Updating the views and signup url pattern

--- mugapp/urls.py
+++ mugapp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register('profile', views.ProfileViewSet)
+router.register('users', views.UserViewSet)
+router.register('posts', views.PostViewSet)
 
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='index'),
+    path('signup/', views.signup, name='signup'),
+    
     
 ]
