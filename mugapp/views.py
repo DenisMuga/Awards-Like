8e814f14bfe7569fc8acb500e59489ddc430e0a0
DenisMuga <denismugah5@gmail.com>
2022-06-12 08:28:02 +0300

    Creating user update profile function

--- mugapp/views.py
+++ mugapp/views.py
@@ -56,3 +56,21 @@ def user_profile(request, username):
         'user_prof': user_prof,
     }
     return render(request, 'mugapp/userprofile.html', params)
+
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    return render(request, 'mugapp/update_profile.html', context)
