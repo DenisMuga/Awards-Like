dacaa6285eb55b4bf2ceec80c628b76a23331698
DenisMuga <denismugah5@gmail.com>
2022-06-12 07:29:23 +0300

    Creating a signup function and its post method

--- mugapp/views.py
+++ mugapp/views.py
@@ -30,3 +30,17 @@ def home(request):
     except Post.DoesNotExist:
         posts = None
     return render(request, 'mugapp/index.html', {'posts': posts, 'form': form, 'random_post': random_post})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
